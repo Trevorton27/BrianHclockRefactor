d5b6b6b673ee4d6d2a3d655649e189d366be9020
Trevor Mearns <Trevor Mearns>
2020-12-15 13:32:03 +0900

    setInterval firing but clock no worky :-(

--- script.js
+++ script.js
@@ -1,104 +1,80 @@
-let dateData = new Date();
+const dateData = new Date();
 
 const monthNames = [
-	'January',
-	'February',
-	'March',
-	'April',
-	'May',
-	'June',
-	'July',
-	'August',
-	'September',
-	'October',
-	'November',
-	'December'
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
 ];
 
-const weekdays = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
+const weekdays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
 
-let month;
-let dayOfWeek;
-let day;
-let hours;
-let hoursMilitary;
-let hoursStandard;
-let hoursDisplay;
-let minutes;
-let seconds;
-let secondsDisplay;
-let milliseconds;
-let universalTimeDifference;
-let period;
 let displayMilitaryTime = false;
 
+const timeDisplayButton = document.getElementById('timeDisplayButton');
+timeDisplayButton.addEventListener('click', timeFormat);
+
 function initiateDate(dateData) {
-	month = monthNames[dateData.getMonth()];
-	dayOfWeek = weekdays[dateData.getDay()];
-	day = dateData.getDate().toString().padStart(2, '0');
+  const month = monthNames[dateData.getMonth()];
+  const dayOfWeek = weekdays[dateData.getDay()];
+  const date = dateData.getDate();
+  const dateDisplay = document.getElementById('date');
+  dateDisplay.textContent = `${dayOfWeek} ${month}, ${date}`;
 }
 
 function initiateTime(dateData) {
-	hours = dateData.getHours() > 12 ? dateData.getHours() - 12 : dateData.getHours();
-	hours = hours === 0 ? hours + 12 : hours;
-	hoursMilitary = dateData.getHours().toString().padStart(2, '0');
-	hoursStandard = hours.toString().padStart(2, '0');
-	hoursDisplay = displayMilitaryTime === true ? hoursMilitary : hoursStandard;
-	minutes = dateData.getMinutes().toString().padStart(2, '0');
-	seconds = dateData.getSeconds();
-	seconds = seconds === 60 ? 0 : seconds;
-	secondsDisplay = seconds.toString().padStart(2, '0');
-	universalTime = dateData.getUTCHours();
-	universalTimeDifference = universalTime - dateData.getHours();
-	period = universalTime - universalTimeDifference >= 12 ? 'PM' : 'AM';
-}
+  const hours = dateData.getHours();
+  const hoursMilitary = hours;
+  const hoursStandard = hours > 12 ? hours - 12 : hours;
 
-function displayTime(hours, minutes, seconds) {
-	document.getElementById('hours').innerHTML = hours;
-	document.getElementById('minutes').innerHTML = minutes;
-	document.getElementById('seconds').innerHTML = seconds;
-	document.getElementById('period').innerHTML = period;
-}
+  const minutes = addLeadingZero(dateData.getMinutes());
+  const seconds = addLeadingZero(dateData.getSeconds());
+  const universalTime = dateData.getUTCHours();
+  const universalTimeDifference = universalTime - dateData.getHours();
+  const period = universalTime - universalTimeDifference >= 12 ? 'PM' : 'AM';
 
-function displayDate(weekday, month, date) {
-	document.getElementById('weekday').innerHTML = weekday;
-	document.getElementById('month').innerHTML = month;
-	document.getElementById('date').innerHTML = date;
+  const twelveHourTime = `${hoursStandard}:${minutes}:${seconds}${period}`;
+  const twentyFourHourTime = `${hoursMilitary}:${minutes}:${seconds}`;
+
+  const clockDisplay = document.getElementById('time');
+
+  displayMilitaryTime === false
+    ? (clockDisplay.textContent = twelveHourTime)
+    : (clockDisplay.textContent = twentyFourHourTime);
+
+  displayMilitaryTime === false
+    ? (timeDisplayButton.textContent = '24 HR')
+    : (timeDisplayButton.textContent = '12 HR');
 }
 
 function timeFormat() {
-	displayMilitaryTime = !displayMilitaryTime;
-	if (displayMilitaryTime === true) {
-		document.getElementById('hours').innerHTML = hoursMilitary;
-		document.getElementById('period').style.display = 'none';
-		hoursDisplay = hoursMilitary;
-		document.getElementById('buttonLabel').innerHTML = '12';
-	} else if (displayMilitaryTime === false) {
-		document.getElementById('hours').innerHTML = hoursStandard;
-		document.getElementById('period').style.display = 'inline';
-		hoursDisplay = hoursStandard;
-		document.getElementById('buttonLabel').innerHTML = '24';
-	}
+  displayMilitaryTime = !displayMilitaryTime;
 }
 
-initiateTime(dateData);
-initiateDate(dateData);
-displayTime(hoursDisplay, minutes, secondsDisplay);
-displayDate(dayOfWeek, month, day);
+function addLeadingZero(time) {
+  return time < 10 ? `0${time}` : time;
+}
 
-setInterval(function() {
-	seconds += 1;
-	if (seconds === 60) {
-		initiateTime(new Date());
-		if (hours === 12 && period === 'AM') {
-			initiateDate(dateData);
-			displayTime(hoursDisplay, minutes, secondsDisplay);
-			displayDate(dayOfWeek, month, day);
-		} else {
-			displayTime(hoursDisplay, minutes, secondsDisplay);
-		}
-	} else {
-		secondsDisplay = seconds.toString().padStart(2, '0');
-		displayTime(hoursDisplay, minutes, secondsDisplay);
-	}
+initiateDate(dateData);
+initiateTime(dateData);
+
+setInterval(() => {
+  initiateTime(dateData);
+  console.log('Im firing');
 }, 1000);
